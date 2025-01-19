7c8567965e39c8f70403fcec9be097231c3193e1
hemanth747 <hemanthkumarreddy747@gmail.com>
2025-01-19 09:21:09 +0530

    wef

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyAMaYg7iWuIybwJscb1Y7Nb-mD_h2lyzxA",
+  authDomain: "test5-e85a1.firebaseapp.com",
+  databaseURL: "https://test5-e85a1-default-rtdb.firebaseio.com",
+  projectId: "test5-e85a1",
+  storageBucket: "test5-e85a1.firebasestorage.app",
+  messagingSenderId: "812596616673",
+  appId: "1:812596616673:web:b306b1cf2670fc3d9f06a0",
+  measurementId: "G-6WP8PQLJZV",
+};
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-
-
+const auth = getAuth(app);
 
 export { app, auth };
